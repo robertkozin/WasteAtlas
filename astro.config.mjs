7bc1c9a73842cb62e6e4b579125b032ce2d3c080
Robert Kozin <robert@robertkozin.com>
2025-05-02 00:41:25 +0800

    local font, more map elements, basic waste page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,43 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  env: {
-      schema: {
-          DIRECTUS_URL: envField.string({context: "server", access: "public", default: "https://directus.thewasteatlas.com"}),
-          DIRECTUS_TOKEN: envField.string({context: "server", access: "secret", optional: true})
-      }
-  },
+    env: {
+        schema: {
+            DIRECTUS_URL: envField.string({ context: "server", access: "public", default: "https://directus.thewasteatlas.com" }),
+            DIRECTUS_TOKEN: envField.string({ context: "server", access: "secret", optional: true })
+        }
+    },
 
-  experimental: {
-      fonts: [
-          {
-              provider: fontProviders.fontshare(),
-              name: "switzer",
-              cssVariable: "--font-switzer"
-          },
-          {
-              provider: fontProviders.fontshare(),
-              name: "erode",
-              cssVariable: "--font-erode",
-          }
-      ]    
-  },
+    experimental: {
+        fonts: [
+            {
+                provider: "local",
+                name: "Switzer",
+                cssVariable: "--font-switzer",
+                variants: [
+                    {
+                        weight: "100 900",
+                        style: "normal",
+                        src: ["./src/assets/fonts/Switzer-Variable.woff2"],
+                    }
+                ]
+            },
+            {
+                provider: "local",
+                name: "Erode",
+                cssVariable: "--font-erode",
+                variants: [
+                    {
+                        weight: "100 900",
+                        style: "normal",
+                        src: ["./src/assets/fonts/Erode-Variable.woff2"]
+                    }
+                ]
+            }
+        ]
+    },
 
-  adapter: node({
-    mode: "standalone"
-  })
+    adapter: node({
+        mode: "standalone"
+    })
 });
--- src/pages/map.json.ts
+++ src/pages/map.json.ts
@@ -5,9 +5,13 @@ import { readItems } from "@directus/sdk";
 export async function GET({params, request}: APIContext) {
     const waste = await directus.request(
         readItems("waste", {
-            fields: ["id", "name", "location", "point"]
+            fields: ["id", "name", "location", "point", "category", "slug"],
+            filter: {}
         })
     )
+
+    // TODO: thumbnail url, category, slug
+    // DEfault coords instead
 
     const geojson = {
         type: "FeatureCollection",
@@ -16,11 +20,13 @@ export async function GET({params, request}: APIContext) {
             id: waste.id,
             geometry: {
                 type: "Point",
-                coordinates: waste.point.coordinates 
+                coordinates: waste.point?.coordinates ?? [-120, 50]
             },
             properties: {
                 name: waste.name,
                 location: waste.location,
+                category: waste.category,
+                slug: waste.slug,
             }
         }))
     }
