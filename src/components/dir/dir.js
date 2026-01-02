6d2f8fdef0ebe6645cce7bee6e64b1aee3739ee3
pedremarta <pedremarta@gmail.com>
2026-01-02 19:50:20 +0100

    addying filter

--- src/components/dir/dir.js
+++ src/components/dir/dir.js
@@ -1,7 +1,14 @@
-console.log ('hello')
-function filter(){
+console.log ('he')
+function applyfilter(){
     let items=document.querySelectorAll ('.item')
     for(const item of items){
         console.log( item.innerHTML)
+        let content = item.innerHTML
+        if (content.includes("industrial")) {
+            continue 
+        } else {
+            item.style.display = "none"  
+        }
     } 
 }
+window.applyfilter = applyfilter
