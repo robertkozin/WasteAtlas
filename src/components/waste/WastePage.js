929b9cb44a53fa823a50fafdd10fa7667d353fc8
lolamarin <lolachamaringa@gmail.com>
2026-01-11 20:04:30 +0100

    primer intento filtro tags

--- src/components/waste/WastePage.js
+++ src/components/waste/WastePage.js
@@ -1,19 +1,19 @@
-
 var project_info = document.querySelector("#project_info")
 var property_info = document.querySelector("#property_info")
 
 //cuando click en boton mostrar el panel de filtro Y cuando click en botón filtrar
-function toggle () {
-  if(project_info.hidden==true){
-    project_info.hidden=false;
-    property_info.hidden=true;
-  } else{
-    project_info.hidden=true;
-    property_info.hidden=false;
+function toggle(tag = "") {
+  console.log("tag:", tag)
+  if (project_info.hidden == true) {
+    project_info.hidden = false
+    property_info.hidden = true
+  } else {
+    project_info.hidden = true
+    property_info.hidden = false
   }
 }
 
-window.toggle=toggle;
+window.toggle = toggle
 //cuando click en BACK ocultar panel de filtro
 
 //cuando click en directory item abrir pagina nueva
